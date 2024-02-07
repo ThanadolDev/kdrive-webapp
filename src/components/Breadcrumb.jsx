021664438f2e38a13f25c4f608053d67b76d9db2
Thanadol Chuesai <141299046+ThanadolDev@users.noreply.github.com>
2024-02-07 13:43:21 +0700

    Update Breadcrumb.jsx

--- src/components/Breadcrumb.jsx
+++ src/components/Breadcrumb.jsx
@@ -10,7 +10,8 @@ export const Breadcrumb = ({shareStatus}) => {
   const {
     folderhistory,
     removeFolderAndSubsequent,
-    removeAllFolderHistory
+    removeAllFolderHistory,
+    permState
   } = useStateContext();
 
   return (
@@ -19,8 +20,8 @@ export const Breadcrumb = ({shareStatus}) => {
         <li class="inline-flex items-center">
           <div
           onClick={() => { 
-            // {shareStatus ? navigate(`/Drive`) : navigate(`/ShareDrive`)}
-            // removeAllFolderHistory()
+            {permState == "Owner" ? navigate(`/Drive`) : navigate(`/ShareDrive`)}
+            removeAllFolderHistory()
           }}
             // href="/Drive"
             class="inline-flex items-center text-xl cursor-pointer font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
@@ -34,7 +35,7 @@ export const Breadcrumb = ({shareStatus}) => {
             >
               <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
             </svg>
-            Home
+            {permState == 'Owner' ? 'My Drive' : 'Share Drive'}
           </div>
         </li>
         <div className=" w-[500px] overflow-x-auto flex inline-flex ">
